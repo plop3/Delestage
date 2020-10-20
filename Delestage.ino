2db51822987a3065114d8c27a39d2e707e0172ef
Serge CLAUS <serge@plop3.fr>
2020-10-20 18:37:14 +0200

    Modifications

--- Delestage.ino
+++ Delestage.ino
@@ -1,17 +1,15 @@
 /*
    Délestage
-   Version 1.0
-   23/09/2020-06/10/2020
+   Version 2.1
+   23/09/2020-20/10/2020
 */
 
 /* TODO
-  Compteurs HC/HP
   Message d'alerte
   Détection coupure/reprise secteur (envoi d'un SMS)
-      -> Revoir la lib téléinfo pour qu'elle ne soit pas bloquante
 */
 
-//#define MY_DEBUG
+#define MY_DEBUG
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
@@ -21,18 +19,16 @@
 #define PINLED 8
 Adafruit_NeoPixel pixels(NUMPIXELS, PINLED, NEO_GRB + NEO_KHZ400);
 
-#include <SimpleTimer.h>
-SimpleTimer timer;
-
 #include <SoftwareSerial.h>
-#include <teleInfo.h>
-#define TI_RX 3
-teleInfo TI( TI_RX );
-#define BUFSIZE 15
-teleInfo_t currentTI;
+#include <LibTeleinfo.h>
+SoftwareSerial Serial1(3, 4);
+TInfo tinfo;
 
 #define MY_RADIO_RF24
 //#define MY_RF24_PA_LEVEL RF24_PA_HIGH
+//#define MY_RF24_PA_LEVEL RF24_PA_MAX
+//#define MY_RF24_DATARATE RF24_1MBPS // fast
+#define MY_RF24_DATARATE RF24_250KBPS // slow
 //#define MY_REPEATER_FEATURE
 #define MY_NODE_ID 6
 #define MY_TRANSPORT_WAIT_READY_MS 5000
@@ -45,6 +41,8 @@ MyMessage current_msg(2, V_CURRENT);    // Intensité
 MyMessage current_PTEC(4, V_STATUS);    // Jour / Nuit
 MyMessage delestage_msg(5, V_STATUS);   // Mode délestage
 MyMessage nbdel_msg(6, V_LEVEL);         // Nombre déléments délestés
+MyMessage HC_msg(7, V_KWH );              // en fait c'est des WH
+MyMessage HP_msg(8, V_KWH );              // en fait c'est des WH
 MyMessage ch1_msg(10, V_STATUS);        // Chambre parents
 MyMessage ch2_msg(11, V_STATUS);        // Chambre Félix
 MyMessage ch3_msg(12, V_STATUS);        // Chambre Léo
@@ -84,6 +82,7 @@ bool Alert = false;
 // Infos
 byte IINST = 120;
 int PAPP = 30000;
+unsigned long HC, HP = 0;
 String PTEC = "HC..";
 
 void before() {
@@ -101,10 +100,12 @@ void before() {
 void presentation() {
   sendSketchInfo("TELEINFO", "1.2.1");
   present(1, S_POWER, "EDF.PUISSANCE");
-  present(2, S_MULTIMETER, "EDF.I.inst");
-  present(4, S_BINARY, "Heure pleine");
+  present(2, S_POWER, "EDF.I.inst");
+  present(4, S_BINARY, "HeurePleine");
   present(5, S_BINARY, "Delestage");
   present(6, S_DUST, "NbElements");
+  present(7, S_POWER, "HeuresCreuses");
+  present(8, S_POWER, "HeuresPleines");
   present(10, S_BINARY, "ChambreP");
   present(11, S_BINARY, "ChambreF");
   present(12, S_BINARY, "ChambreL");
@@ -113,14 +114,18 @@ void presentation() {
 }
 
 void setup() {
-  timer.setTimeout(8000, sendInitialData);
+  Serial1.begin(1200);
+  tinfo.init();
+  tinfo.attachData(readData);
+  tinfo.attachNewFrame(delestage);
+  delay(2000);
+  sendInitialData();
 }
 
 void loop() {
-  timer.run();
   // Lecture de l'intensité actuelle
-  readData();
-  delestage();
+  if ( Serial1.available() )
+    tinfo.process(Serial1.read());
 }
 
 void receive(const MyMessage &message)
@@ -135,26 +140,39 @@ void receive(const MyMessage &message)
   }
 }
 
-void readData()
+void readData(ValueList * me, uint8_t  flags)
 {
-  currentTI = TI.get();
+  String rep = String(me->name);
+  unsigned long valeur = String(me->value).toInt();
   // Reception des valeurs
+  if (flags & (TINFO_FLAGS_UPDATED | TINFO_FLAGS_ADDED)) {
+    if (rep == "IINST") {
+      IINST = valeur;
+      send(current_msg.set(IINST));
+    }
+    else if (rep == "PAPP") {
+      PAPP = valeur;
+      send(papp_msg.set(PAPP));
+    }
+    else if (rep == "PTEC") {
+      PTEC = me->value;
+      send(current_PTEC.set(PTEC == "HC.." ? 0 : 1));
+    }
+    else if ((rep == "HCHC") && (valeur != HC)) {
+      HC = valeur;
+      Serial.println(float(valeur)/1000);
+      send(HC_msg.set(float(valeur)/1000,3));
+    }
+    else if ((rep == "HCHP" ) && (valeur != HP)) {
+      HP = valeur;
+      send(HP_msg.set(float(valeur)/1000,3));
+      Serial.println(float(valeur)/1000);
+    }
+  }
   //  IMAX = currentTI.IMAX;
   //  HC = currentTI.HC_HC;
   //  HP = currentTI.HC_HP;
-  if (PTEC != currentTI.PTEC) {
-    PTEC = currentTI.PTEC;
-    send(current_PTEC.set(PTEC == "HC.." ? 0 : 1));
-  }
-  if (PAPP != currentTI.PAPP) {
-    PAPP = currentTI.PAPP;
-    send(papp_msg.set(PAPP));
-  }
 
-  if (IINST != currentTI.IINST) {
-    IINST = currentTI.IINST;
-    send(current_msg.set(IINST));
-  }
 }
 
 void sendInitialData() {
@@ -167,7 +185,12 @@ void sendInitialData() {
   send(nbdel_msg.set(0));
 }
 
+int nbt = 0;
 void delestage() {
+  // délestage toutes les 2 trames
+  nbt++;
+  if (nbt < 2) return;
+  nbt = 0;
   byte j;
   // Intensité < Max
   if (IINST <= IDEL) {
