f3d0303d10a0a5bff5f626c237454cdb2d10b757
Serge <serge.claus@free.fr>
2022-11-10 12:54:45 +0100

    Suppression reset, ajout watchdof

--- Delestage.ino
+++ Delestage.ino
@@ -6,16 +6,20 @@
 */
 
 // Pins
-#define RESETPIN 2 // Commande de reset de l'Arduino
+//#define RESETPIN 5  // Commande de reset de l'Arduino
 
 //#define MY_DEBUG
 #include <SimpleTimer.h>
 SimpleTimer timer;
-#define TPSSTART 120000   // Délai avant envoi des mesures
+#define TPSSTART 120000  // Délai avant envoi des mesures
 
+// Watchdog
+#include <avr/wdt.h>
+
+// LED
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
-#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#include <avr/power.h>  // Required for 16 MHz Adafruit Trinket
 #endif
 #define NUMPIXELS 1
 #define PINLED 8
@@ -38,20 +42,20 @@ TInfo tinfo;
 #include <MySensors.h>
 
 //MyMessage kwh_msg(1, V_KWH);
-MyMessage papp_msg(1, V_WATT);          // Puissance
-MyMessage current_msg(2, V_CURRENT);    // Intensité
-MyMessage current_PTEC(4, V_STATUS);    // Jour / Nuit
-MyMessage delestage_msg(5, V_STATUS);   // Mode délestage
-MyMessage nbdel_msg(6, V_LEVEL);         // Nombre déléments délestés
-MyMessage HC_msg(7, V_KWH );              // en fait c'est des WH
-MyMessage HP_msg(8, V_KWH );              // en fait c'est des WH
-MyMessage Alerte_msg(9, V_STATUS);          // Alerte délestage maximum
-MyMessage ch1_msg(10, V_STATUS);        // Chambre parents
-MyMessage ch2_msg(11, V_STATUS);        // Chambre Félix
-MyMessage ch3_msg(12, V_STATUS);        // Chambre Léo
-MyMessage ch4_msg(13, V_STATUS);        // Salon
-MyMessage ch7_msg(14, V_STATUS);        // Cumulus
-MyMessage ch5_msg(15, V_STATUS);        // Chambre Léo2
+MyMessage papp_msg(1, V_WATT);         // Puissance
+MyMessage current_msg(2, V_CURRENT);   // Intensité
+MyMessage current_PTEC(4, V_STATUS);   // Jour / Nuit
+MyMessage delestage_msg(5, V_STATUS);  // Mode délestage
+MyMessage nbdel_msg(6, V_LEVEL);       // Nombre déléments délestés
+MyMessage HC_msg(7, V_KWH);            // en fait c'est des WH
+MyMessage HP_msg(8, V_KWH);            // en fait c'est des WH
+MyMessage Alerte_msg(9, V_STATUS);     // Alerte délestage maximum
+MyMessage ch1_msg(10, V_STATUS);       // Chambre parents
+MyMessage ch2_msg(11, V_STATUS);       // Chambre Félix
+MyMessage ch3_msg(12, V_STATUS);       // Chambre Léo
+MyMessage ch4_msg(13, V_STATUS);       // Salon
+MyMessage ch7_msg(14, V_STATUS);       // Cumulus
+MyMessage ch5_msg(15, V_STATUS);       // Chambre Léo2
 //MyMessage Depassement_msg(20, V_STATUS); // Dépassement intensité d'alerte
 //
 
@@ -61,13 +65,13 @@ MyMessage ch5_msg(15, V_STATUS);        // Chambre Léo2
 struct S {
   byte sortie;
   byte intensite;
-  bool on;        // Etat relais ON (0 ou 1)
-  bool etat;      // Element coupé ou activé par un interrupteur
-  bool delest;    // Element délesté
+  bool on;      // Etat relais ON (0 ou 1)
+  bool etat;    // Element coupé ou activé par un interrupteur
+  bool delest;  // Element délesté
 };
 
 // Priorités
-byte prio[nbElements] = {2, 1, 0, 3, 5, 4};
+byte prio[nbElements] = { 2, 1, 0, 3, 5, 4 };
 
 // Sorties par ordre (Pin, Intensité, Etat marche (pas de délestage), Coupé/allumé (inter))
 /*
@@ -78,15 +82,15 @@ byte prio[nbElements] = {2, 1, 0, 3, 5, 4};
   5 Cumulus
   6 Petit radiateur Léo
 */
-struct S IO[nbElements] = {{16, 9, 0, 1, 0}, {17, 7, 0, 1, 0}, {19, 7, 0, 1, 0}, {18, 7, 0, 1, 0}, {5, 8, 1, 1, 0}, {15, 5, 0, 1, 0}};
+struct S IO[nbElements] = { { 16, 9, 0, 1, 0 }, { 17, 7, 0, 1, 0 }, { 19, 7, 0, 1, 0 }, { 18, 7, 0, 1, 0 }, { 5, 8, 1, 1, 0 }, { 15, 5, 0, 1, 0 } };
 
 // Intensité maxi
-byte IDEL = 30;   // Intensité maxi avant délestage
-byte IALERT = 35; // Intensité d'alerte, on déleste tout d'un coup
-byte IURGENCE = 40; // Intensité maximum, on n'attend pas une deuxième mesure
+byte IDEL = 30;      // Intensité maxi avant délestage
+byte IALERT = 35;    // Intensité d'alerte, on déleste tout d'un coup
+byte IURGENCE = 40;  // Intensité maximum, on n'attend pas une deuxième mesure
 bool Alert = false;
 bool OldAlert = !Alert;
-bool Mes = false;   // Autorisation d'envoi des mesures
+bool Mes = false;  // Autorisation d'envoi des mesures
 
 // Infos
 byte IINST = 120;
@@ -95,6 +99,8 @@ unsigned long HC, HP = 0;
 String PTEC = "HC..";
 
 void before() {
+  //digitalWrite(RESETPIN, HIGH);
+  //pinMode(RESETPIN, OUTPUT);
   // Coupure de toutes les sorties
   for (int i = 0; i < nbElements; i++) {
     //Serial.println(IO[i].sortie);
@@ -103,7 +109,7 @@ void before() {
   }
   pixels.begin();
   pixels.clear();
-  LED(100, 100, 100); // Blanc
+  LED(100, 100, 100);  // Blanc
 }
 
 void presentation() {
@@ -122,11 +128,13 @@ void presentation() {
   present(13, S_BINARY, "Salon");
   present(14, S_BINARY, "Cumulus");
   present(15, S_BINARY, "ChambreL2");
-  present(20, S_BINARY, "ResetDelestage");
+  //present(20, S_BINARY, "ResetDelestage");
 }
 
 void setup() {
+  wdt_disable();
   Serial1.begin(1200);
+  wdt_enable(WDTO_8S);
   timer.setTimeout(TPSSTART, startMesure);
   tinfo.init();
   tinfo.attachData(readData);
@@ -138,37 +146,35 @@ void setup() {
 void loop() {
   // Lecture de l'intensité actuelle
   if (!Mes) timer.run();
-  if ( Serial1.available() )
+  wdt_reset();
+  if (Serial1.available())
     tinfo.process(Serial1.read());
 }
 
-void receive(const MyMessage &message)
-{
+void receive(const MyMessage &message) {
   if (message.getType() == V_STATUS) {
-    if (message.getSensor() == 20)
-    {
+    /*
+    if (message.getSensor() == 20) {
       // Reset de l'Arduino
       pinMode(RESETPIN, OUTPUT);
       digitalWrite(RESETPIN, LOW);
-    } 
-    else if (message.getSensor() > 9) {
+    } else */
+    if (message.getSensor() > 9) {
       // ON/OFF
       byte index = message.getSensor() - 10;
-	  if (message.getBool()) {
-		if (IO[index].intensite + IINST <= IDEL) digitalWrite(IO[index].sortie, IO[index].on);  
-	  }
-	  else {
-		digitalWrite(IO[index].sortie, !IO[index].on);  
-	  }
+      if (message.getBool()) {
+        if (IO[index].intensite + IINST <= IDEL) digitalWrite(IO[index].sortie, IO[index].on);
+      } else {
+        digitalWrite(IO[index].sortie, !IO[index].on);
+      }
       //digitalWrite(IO[index].sortie, message.getBool() ? IO[index].on : !IO[index].on);
       IO[index].etat = message.getBool();
-	  updateLed();
+      updateLed();
     }
   }
 }
 
-void readData(ValueList * me, uint8_t  flags)
-{
+void readData(ValueList *me, uint8_t flags) {
   String rep = String(me->name);
   unsigned long valeur = String(me->value).toInt();
   // Reception des valeurs
@@ -176,20 +182,16 @@ void readData(ValueList * me, uint8_t  flags)
     if (rep == "IINST") {
       IINST = valeur;
       send(current_msg.set(IINST));
-    }
-    else if (rep == "PAPP") {
+    } else if (rep == "PAPP") {
       PAPP = valeur;
       send(papp_msg.set(PAPP));
-    }
-    else if (rep == "PTEC") {
+    } else if (rep == "PTEC") {
       PTEC = me->value;
       send(current_PTEC.set(PTEC == "HC.." ? 0 : 1));
-    }
-    else if ((rep == "HCHC") && (valeur > HC)) {
+    } else if ((rep == "HCHC") && (valeur > HC)) {
       HC = valeur;
-	    if (Mes) send(HC_msg.set(float(valeur) / 1000, 3));
-    }
-    else if ((rep == "HCHP" ) && (valeur > HP)) {
+      if (Mes) send(HC_msg.set(float(valeur) / 1000, 3));
+    } else if ((rep == "HCHP") && (valeur > HP)) {
       HP = valeur;
       if (Mes) send(HP_msg.set(float(valeur) / 1000, 3));
     }
@@ -220,14 +222,14 @@ void delestage() {
   if (IINST <= IDEL) {
     if (Alert) {
       Alert = false;
-	  updateLed();
+      updateLed();
     }
     // On teste par ordre de priorité si la sortie est coupée
     for (int i = 0; i < nbElements; i++) {
       j = prio[i];
       if (digitalRead(IO[j].sortie) == !IO[j].on && (IO[j].intensite + IINST <= IDEL) && IO[j].etat) {
         digitalWrite(IO[j].sortie, IO[j].on);
-		updateLed();
+        updateLed();
         break;
       }
     }
@@ -238,18 +240,17 @@ void delestage() {
       j = prio[i];
       if (digitalRead(IO[j].sortie) == IO[j].on) {
         digitalWrite(IO[j].sortie, !IO[j].on);
-		updateLed();
+        updateLed();
         break;
       }
     }
-  }
-  else {
+  } else {
     for (int i = 0; i < nbElements; i++) {
       if (digitalRead(IO[i].sortie) == IO[i].on) {
         digitalWrite(IO[i].sortie, !IO[i].on);
       }
     }
-	updateLed();
+    updateLed();
     Alert = true;
   }
   if (Alert != OldAlert) {
@@ -259,13 +260,15 @@ void delestage() {
 }
 
 void updateLed() {
-	int nbd=0;
-	for (int i = 0; i < nbElements; i++) {
-		if (digitalRead(IO[i].sortie) != IO[i].on && IO[i].etat) nbd++;
-	}	
-	send(nbdel_msg.set(nbd));
-	send(delestage_msg.set(nbd==0 ? 0 : 1));
-	if (nbd==0) LED(75, 0, 0); else if (nbd< nbElements) LED(37, 70, 5); else LED(0,75,0);
+  int nbd = 0;
+  for (int i = 0; i < nbElements; i++) {
+    if (digitalRead(IO[i].sortie) != IO[i].on && IO[i].etat) nbd++;
+  }
+  send(nbdel_msg.set(nbd));
+  send(delestage_msg.set(nbd == 0 ? 0 : 1));
+  if (nbd == 0) LED(75, 0, 0);
+  else if (nbd < nbElements) LED(37, 70, 5);
+  else LED(0, 75, 0);
 }
 
 void LED(byte V, byte R, byte B) {
@@ -275,5 +278,5 @@ void LED(byte V, byte R, byte B) {
 
 void startMesure() {
   // Démarrage des mesures
-  Mes=true;
+  Mes = true;
 }
